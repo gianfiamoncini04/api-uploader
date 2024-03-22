0ba61d76b15b7de154a00a52890e7631ef434379
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2024-03-22 01:01:48 -0300

    miss gui front end

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,72 @@
 import fastify from "fastify";
-
+import cors from "@fastify/cors";
+import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { r2 } from './cloudflare';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { z } from "zod"
+import { randomUUID } from "crypto";
+import { PrismaClient } from '@prisma/client'
+ 
 const app = fastify()
 
-app.get('/', (req, res) => {
-    return "DEU CERTO"
+const prisma = new PrismaClient()
+
+app.register(cors, {
+    origin: '*',
+})
+
+app.post('/uploads', async (request) => {
+    const uploadBodySchema = z.object({
+        name: z.string().min(1),
+        contentType: z.string().regex(/\w+\/[-+.\w]+/)
+    })
+
+    const {name, contentType} = uploadBodySchema.parse(request?.body)
+
+    const fileKey = randomUUID().concat('-').concat(name)
+
+    const signedUrl = await getSignedUrl(r2, 
+        new PutObjectCommand({
+            Bucket: 'uploader-app',
+            Key: fileKey,
+            ContentType: contentType,
+        }),
+        {expiresIn: 600} 
+    )
+
+    const file = await prisma.file.create({
+        data: {
+            name,
+            contentType,
+            key: fileKey,
+        }
+    })
+
+    return {signedUrl, fileId: file.id}
+})
+
+app.get('/uploads/:id', async (request) => {
+    const GetFileParam = z.object({
+        id: z.string().cuid()
+    })
+
+    const {id} = GetFileParam.parse(request.params)
+
+    const file = await prisma.file.findUniqueOrThrow({
+        where: {
+            id,
+        }
+    })
+
+    const signedUrl = await getSignedUrl(r2, 
+        new GetObjectCommand({
+            Bucket: 'uploader-app',
+            Key: file.key,
+        }),
+        {expiresIn: 600} 
+    )
+
+    return {signedUrl}
 })
 
 async function Start() {
